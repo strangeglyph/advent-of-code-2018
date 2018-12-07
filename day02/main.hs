ec3d019547794f0e65d29d4970c0730a8f66935b
boreeas <malte.schuetze@fgms.de>
2018-12-07 19:02:28 +0100

    Day 02, Part 02

--- day02/main.hs
+++ day02/main.hs
@@ -2,11 +2,37 @@ import Data.List
 
 main = do
     input <- readFile "input"
-    let solution = solveA input
-    putStrLn $ solution
+    putStrLn $ solveA input
+    putStrLn $ solveB input
 
 solveA :: String -> String
 solveA = show . checksum . lines
+
+solveB :: String -> String
+solveB = uncurry same . findMatching . lines
+
+findMatching :: [String] -> (String,String)
+findMatching = head . filter (uncurry isSimilar) . pairs
+
+isSimilar :: String -> String -> Bool
+isSimilar a b = numDifferent == 1
+    where numDifferent = length $ different a b
+
+same :: String -> String -> String
+same [] [] = []
+same (x:xs) (y:ys)
+    | x == y    = x : same xs ys
+    | otherwise = same xs ys
+
+different :: String -> String -> [(Char, Char)]
+different [] [] = []
+different (x:xs) (y:ys)
+    | x /= y    = (x,y) : different xs ys
+    | otherwise = different xs ys
+
+pairs :: [a] -> [(a,a)]
+pairs [] = []
+pairs (x:xs) = map (\y -> (x,y)) xs ++ pairs xs
 
 checksum :: [String] -> Int
 checksum labels = (countN 3 labels) * (countN 2 labels)
