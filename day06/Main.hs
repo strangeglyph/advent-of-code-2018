a063a4631371e0f12ac2637a24ef4831d5e788ae
boreeas <malte.schuetze@fgms.de>
2018-12-16 21:26:28 +0100

    Part 2

--- day06/Main.hs
+++ day06/Main.hs
@@ -15,7 +15,7 @@ solveA inpt = show $ maximum $ map length $ group $ sort $ filter (not . flip el
           bIds = borderIds coords
 
 solveB :: String -> String
-solveB inpt = undefined
+solveB = show . regionSize . parseAll
 
 
 isInfinite :: [Coord] -> Coord -> Bool
@@ -29,12 +29,21 @@ borderIds cs = nub $ catMaybes $ map (closest cs) $ nub  $ horizontal ++ vertica
 field :: [Coord] -> [Coord]
 field cs = catMaybes $ map (closest cs) $ liftM2 Coord [0..maxX cs] [0..maxY cs]
 
+
 closest :: [Coord] -> Coord -> Maybe Coord
 closest coords p = case closestCoords of
         [x] -> Just x
         _   -> Nothing
     where minDist = minimum $ map (manhattan p) coords
           closestCoords = filter (\x -> manhattan p x == minDist) coords
+
+
+regionSize :: [Coord] -> Int
+regionSize cs = length $ filter (<10000) $ map (cumDist cs) $ liftM2 Coord [0..maxX cs] [0..maxY cs]
+
+
+cumDist :: [Coord] -> Coord -> Int
+cumDist coords p = sum $ map (manhattan p) coords
 
 manhattan :: Coord -> Coord -> Int
 manhattan p1 p2 = abs (x p1 - x p2) + abs (y p1 - y p2)
